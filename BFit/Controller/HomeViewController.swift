442fb1e5ae53fc29bc875dbadfb6c41d9363c5f7
Cody-Price <ctprice11@gmail.com>
2019-02-19 13:37:27 -0700

    Add tabBar transition extension

--- BFit/Controller/HomeViewController.swift
+++ BFit/Controller/HomeViewController.swift
@@ -12,5 +12,22 @@ class HomeViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        delegate = self
+    }
+    
+}
+
+extension HomeViewController: UITabBarControllerDelegate  {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        
+        guard let fromView = selectedViewController?.view, let toView = viewController.view else {
+            return false // Make sure you want this as false
+        }
+        
+        if fromView != toView {
+            UIView.transition(from: fromView, to: toView, duration: 0.3, options: [.transitionCrossDissolve], completion: nil)
+        }
+        
+        return true
     }
 }
